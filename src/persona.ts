8624a662529854dc3d2e6349fb54afe33dde06b7
Adrian Melian Fdez <alu0100974652@ull.edu.es>
2022-03-14 10:09:07 +0000

    Test getter apellidos

--- src/persona.ts
+++ src/persona.ts
@@ -30,21 +30,4 @@ export class Persona
         return this.nombre;
     }
 
-    public getApellidos ()
-    {
-        return this.apellidos;
-    }
-
-    public getFechaNacimiento ()
-    {
-        return this.fechaNacimiento;
-    }
-
-    public getGenero ()
-    {
-        return this.genero;
-    }
-
-
-
 }
--- tests/persona.spec.ts
+++ tests/persona.spec.ts
@@ -6,6 +6,10 @@ describe('Pruebas para la clase persona getNombre', () => {
     const persona = new Persona('Pepe', 'Papa Pipi', '12-12-1980', 'Masculino');
     const nombrePersona = persona.getNombre();
     expect(nombrePersona).to.be.equal('Pepe');
-
 });
 
+describe('Pruebas para la calse persona getApellidos', () => {
+    const persona = new Persona('Pepe', 'Papa Pipi', '12-12-1980', 'Masculino');
+    const apellidosPersona = persona.getApellidos();
+    expect(apellidosPersona).to.be.equal('Papa Pipi');
+})
