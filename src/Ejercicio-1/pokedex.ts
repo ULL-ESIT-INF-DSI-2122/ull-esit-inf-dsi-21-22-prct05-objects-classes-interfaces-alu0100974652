b72263cb3f6ec1f4987492864119c83bfae25175
Adrian Melian Fdez <alu0100974652@ull.edu.es>
2022-03-20 12:55:52 +0000

    Se puede buscar un pokemon en la pokedex

--- src/Ejercicio-1/pokedex.ts
+++ src/Ejercicio-1/pokedex.ts
@@ -1,7 +1,10 @@
 /**
  * Clase Pokedex
  * @class Pokedex
- * 
+ * @param nPokemons arrays que contendrá los distintos pokemons de la pokedex
+ * @method getPokemonPosition retorna el pokemon en el número de la pokedex
+ * @method addPokemon añade un pokemon al final de la pokedex
+ * @method searchPokemon busca un pokemon dentro de la pokedex
  */
 
 import { Pokemon } from "./pokemon";
@@ -22,5 +25,25 @@ export class Pokedex
     public addPokemon(nuevoPokemon: Pokemon)
     {
         this.nPokemons.push(nuevoPokemon);
+    }
+
+    public tamanioPokedex()
+    {
+        return this.nPokemons.length;
+    }
+
+    public searchPokemon(buscarPokemon: Pokemon)
+    {
+        let encontrado :Boolean = false;
+
+        for(let i = 0; i < this.nPokemons.length; i++)
+        {
+            if(buscarPokemon == this.nPokemons[i])
+            {
+                encontrado = true;
+            }
+        }
+
+        return encontrado;
     }
 }
--- tests/Ejercicio-1-Test/pokedex.spec.ts
+++ tests/Ejercicio-1-Test/pokedex.spec.ts
@@ -7,6 +7,7 @@ describe('Pruebas para comprobar la clase Pokedex:', () => {
     const Bulbasaur = new Pokemon('Bulbasaur', 6.9, 0.7, 'Planta', [49, 49, 45, 45]);
     const Charmander = new Pokemon('Charmander', 8.5, 0.6, 'Fuego', [52,43,65,39]);
     const Squirtle = new Pokemon('Squirtle', 9, 0.5, 'Agua', [46,65,43,44]);
+    const Pikachu = new Pokemon('Pikachu',6,0.4,'Electrico',[55,40,90,35]);
 
     const PokedexKanto = new Pokedex([Bulbasaur, Charmander]);
 
@@ -21,8 +22,7 @@ describe('Pruebas para comprobar la clase Pokedex:', () => {
 
     it('Se puede buscar un Pokemon en la Pokedex', () => {
         expect(PokedexKanto.searchPokemon(Bulbasaur)).to.be.eq(true);
-        expect(PokedexKanto.searchPokemon(Squirtle)).to.be.eq(false);
+        expect(PokedexKanto.searchPokemon(Pikachu)).to.be.eq(false);
     });
-
 
 });
