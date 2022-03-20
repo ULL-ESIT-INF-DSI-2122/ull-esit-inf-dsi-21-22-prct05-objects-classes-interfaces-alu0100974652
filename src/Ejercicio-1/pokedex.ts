d1df3866506da23d49fea7d0bec75042f629b249
Adrian Melian Fdez <alu0100974652@ull.edu.es>
2022-03-20 12:28:14 +0000

    Creando la clase Pokedex con sus tests

---
+++ src/Ejercicio-1/pokedex.ts
@@ -0,0 +1,13 @@
+/**
+ * Clase Pokedex
+ * @class Pokedex
+ * 
+ */
+
+import { Pokemon } from "./pokemon";
+
+export class Pokedex
+{
+    private nPokemons: Pokemon[] = [];
+    constructor() {};
+}
---
+++ tests/Ejercicio-1-Test/pokedex.spec.ts
@@ -0,0 +1,11 @@
+import 'mocha';
+import { expect } from 'chai';
+import { Pokemon } from '../../src/Ejercicio-1/pokemon';
+import {Pokedex} from '../../src/Ejercicio-1/pokedex';
+
+describe('Pruebas para comprobar la clase Pokedex:', () => {
+    const Bulbasaur = new Pokemon('Bulbasaur', 6.9, 0.7, 'Planta', [49, 49, 45, 45]);
+    const Charmander = new Pokemon('Charmander', 8.5, 0.6, 'Fuego', [52,43,65,39]);
+    const Squirtle = new Pokemon('Squirtle', 9, 0.5, 'Agua', [46,65,43,44]);
+
+});
